caeb5718744332db2add98429a35ff36191bbcde
Röbke Geenen <robke@insanity.insaniac.net>
2018-01-15 21:03:35 +0100

    Outer loop of apen sucessfully parallelized, initial effort to parallelize inner loop started.

--- apen.cu
+++ apen.cu
@@ -1,16 +1,12 @@
 #include "eeg.h"
 
-__device__ double atomicAdd(double* address, double val)
-{
-  unsigned long long int* address_as_ull =
-    (unsigned long long int*)address;
+__device__ double atomicAdd(double* address, double val){
+  unsigned long long int* address_as_ull = (unsigned long long int*)address;
   unsigned long long int old = *address_as_ull, assumed;
-  do {
+  do{
     assumed = old;
-    old = atomicCAS(address_as_ull, assumed,
-                    __double_as_longlong(val +
-                                         __longlong_as_double(assumed)));
-  } while (assumed != old);
+    old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val + __longlong_as_double(assumed)));
+  }while(assumed != old);
   return __longlong_as_double(old);
 }
 
@@ -18,35 +14,53 @@ __global__
 void apen_correlation (int np, int32_t *x, unsigned int m, double r, double *result)
 {
   bool set;
-  unsigned int count;
+  extern __shared__ unsigned int count[];
   __shared__ double sum;
   sum = 0;
-  unsigned i = threadIdx.x;
+  unsigned int j = threadIdx.x;
+  unsigned int i = blockIdx.x;
   //printf("BlockID: %d\n", i);
   //for(unsigned int i = 0; i <= np - m; i++){
-    count = 0;
-    __syncthreads();
-    for(unsigned int j = 0; j <= np - m; j++){
-      set = false;
-      for(unsigned int k = 0; k < m; k++){
-        if(abs(x[i + k] - x[j + k]) > r){
-          set = true;
-          break;
-        }
-      }
-      if(!set){
-        count++;
-      }
-    }
-    atomicAdd(&sum, ((double)count) / ((double)np - m + 1));
-    //sum += ((double)count) / ((double)np - m + 1);
-    //if(threadIdx.x == 0){
-      printf("Sum: %lf\n", sum);
-      //}
-    //}
+  count[i] = 0;
+  //for(unsigned int j = 0; j <= np - m; j++){
   __syncthreads();
-  if(i == 0){
-  *result = sum / ((double)np - m + 1);
+  set = false;
+  for(unsigned int k = 0; k < m; k++){
+    if(abs(x[i + k] - x[j + k]) > r){
+      set = true;
+       if(blockIdx.x == 0){
+        //printf("Test A\n");
+      }
+      break;
+    }
+  }
+  if(blockIdx.x == 0){
+  printf("Test C: %s\n", set ? "true" : "false");
+}
+  if(!set){
+    //count[i]++;
+    atomicAdd(&count[i], 1);
+    if(blockIdx.x == 0){
+      //printf("Test B\n");
+      //printf("B: %d\n", count[i]);
+    }
+  }
+
+  
+  //}
+  __syncthreads();
+  if(blockIdx.x == 0 && threadIdx.x == 0){
+    printf("Count: %d\n", count[i]);
+  }
+  if(j == 0){
+    atomicAdd(&sum, ((double)count[i]) / ((double)np - m + 1));
+  }
+  //sum += ((double)count) / ((double)np - m + 1);
+  
+  //}
+  __syncthreads();
+  if(i == 0 && j == 0){
+    *result = sum / ((double)np - m + 1);
   }
 }
 
@@ -58,8 +72,8 @@ void apen(int np, int32_t *x, float *a, unsigned int m, double r)
   cudaCheckError(cudaMalloc(&dev_inter1, sizeof(double)));
   cudaCheckError(cudaMalloc(&dev_inter2, sizeof(double)));
   cudaCheckError(cudaMemcpy(dev_x, x, np*sizeof(int32_t), cudaMemcpyHostToDevice));
-  apen_correlation<<<1, (np - m + 1)>>>(np, dev_x, m, r, dev_inter1);
-  apen_correlation<<<1, (np - m + 1)>>>(np, dev_x, m + 1, r, dev_inter2);
+  apen_correlation<<<(np - m + 1), (np - m + 1), (np - m + 1)*sizeof(unsigned int)>>>(np, dev_x, m, r, dev_inter1);
+  apen_correlation<<<(np - m + 1), (np - m + 1), (np - m + 1)*sizeof(unsigned int)>>>(np, dev_x, m + 1, r, dev_inter2);
   cudaCheckError(cudaDeviceSynchronize());
   cudaCheckError(cudaMemcpy(&inter1, dev_inter1, sizeof(double), cudaMemcpyDeviceToHost));
   cudaCheckError(cudaMemcpy(&inter2, dev_inter2, sizeof(double), cudaMemcpyDeviceToHost));
