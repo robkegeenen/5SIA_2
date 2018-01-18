a66dc325f73a506d7f056316cd191d8acda4ccd4
Röbke Geenen <robke@insanity.insaniac.net>
2018-01-18 16:40:28 +0100

    Removed empty line

--- eeg.cu
+++ eeg.cu
@@ -35,7 +35,6 @@ int main(int argc, char *argv[]) {
   blocksize = (prop.maxThreadsPerBlock > prop.maxThreadsDim[0]) ? prop.maxThreadsDim[0] : prop.maxThreadsPerBlock;
   //printf("Blocksize: %d\n", blocksize);
 
-  
   doNothingFucker<<<1, 1>>>();
 
   read_data(x, CHANNELS, DATAPOINTS);
