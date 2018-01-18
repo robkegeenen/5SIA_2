ceb9785c2bfcd1c8afca3ad4b7ddba8a11ba4137
Röbke Geenen <robke@insanity.insaniac.net>
2018-01-18 11:57:33 +0100

    Changed i loop in apen.cu to use blocks instead of threads.

--- apen.cu
+++ apen.cu
@@ -13,14 +13,14 @@ __device__ double atomicAdd(double* address, double val){
 __global__
 void apen_correlation (int np, int32_t *x, unsigned int m, double r, double *result)
 {
+  unsigned int i = blockIdx.x;
   bool set;
   unsigned int count;
-  __shared__ double sum;
-  sum = 0;
-  unsigned int i = threadIdx.x;
+  if(i == 0){
+    *result = 0;
+  }
   //for(unsigned int i = 0; i <= np - m; i++){
   count = 0;
-  __syncthreads();
   for(unsigned int j = 0; j <= np - m; j++){
     set = false;
     for(unsigned int k = 0; k < m; k++){
@@ -34,29 +34,27 @@ void apen_correlation (int np, int32_t *x, unsigned int m, double r, double *res
     }
   }
   //sum += ((double)count) / ((double)np - m + 1);
-  atomicAdd(&sum, ((double)count) / ((double)np - m + 1));
+  atomicAdd(result, ((double)count) / ((double)np - m + 1));
   //}
-  __syncthreads();
-  if(i == 0){
-    *result = sum / ((double)np - m + 1);
-  }
 }
 
 void apen(int np, int32_t *x, float *a, unsigned int m, double r)
 {
   double *dev_inter1, inter1, *dev_inter2, inter2;
   int32_t *dev_x;
+  int length1 = np - (m + 0) + 1;
+  int length2 = np - (m + 1) + 1;
   cudaCheckError(cudaMalloc(&dev_x, np*sizeof(int32_t)));
   cudaCheckError(cudaMalloc(&dev_inter1, sizeof(double)));
   cudaCheckError(cudaMalloc(&dev_inter2, sizeof(double)));
   cudaCheckError(cudaMemcpy(dev_x, x, np*sizeof(int32_t), cudaMemcpyHostToDevice));
-  apen_correlation<<<1, (np - (m + 0) + 1)>>>(np, dev_x, m + 0, r, dev_inter1);
-  apen_correlation<<<1, (np - (m + 1) + 1)>>>(np, dev_x, m + 1, r, dev_inter2);
+  apen_correlation<<<length1, 1>>>(np, dev_x, m + 0, r, dev_inter1);
+  apen_correlation<<<length2, 1>>>(np, dev_x, m + 1, r, dev_inter2);
   cudaCheckError(cudaDeviceSynchronize());
   cudaCheckError(cudaMemcpy(&inter1, dev_inter1, sizeof(double), cudaMemcpyDeviceToHost));
   cudaCheckError(cudaMemcpy(&inter2, dev_inter2, sizeof(double), cudaMemcpyDeviceToHost));
   cudaCheckError(cudaFree(dev_x));
   cudaCheckError(cudaFree(dev_inter1));
   cudaCheckError(cudaFree(dev_inter2));
-  *a = log(inter1 / inter2);
+  *a = log((inter1 / ((double)length1)) / (inter2 / ((double)length2)));
 }
